187e9d4a3d1824c4699eb0d9372bd37d2d215b4c
NewFate <dem.serega2001.sd@gmail.com>
2021-08-02 12:54:43 +0000

    Remove all stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-42^wch_5#x)$6joiq96i&a47p(2_wiy+=40jsi0w-%4ad9azt!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newfate35.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
