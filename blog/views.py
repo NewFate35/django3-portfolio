78e88e4ece9275829a931675e2e37e9c94a92452
NewFate <dem.serega2001@mail.ru>
2021-08-04 19:37:41 +0500

    Url on Title

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.all().order_by('date')
+    blogs = Blog.objects.all().order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
